9612e639632bcb7d0669b55e220fa51742cdf47c
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-05-19 17:06:25 +0300

    Сделать запускаемый класс, содержащий функцию public static void main(String[] args) {...}

--- sandbox/src/main/java/my/pkg/olegclass/MyFirstProgram.java
+++ sandbox/src/main/java/my/pkg/olegclass/MyFirstProgram.java
@@ -1,8 +1,18 @@
 package my.pkg.olegclass;
 
 public class MyFirstProgram {
-
+    public static double distance(Point p1, Point p2) {
+        double dx = p1.x - p2.x;
+        double dy = p1.y - p2.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        Point p1 = new Point(34, 67);
+        Point p2 = new Point(38, 69);
+        double dist = distance(p1, p2);
+        System.out.println("Расстояние между точками  =  " + dist);
+
     }
+
 }
