951a5d08cf81ae4afef518ab480eae318055ed7d
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-05-19 13:27:47 +0300

    Создана функция вычисляющая растояние между двумя точками

---
+++ sandbox/src/main/java/my/pkg/olegclass/Point.java
@@ -0,0 +1,16 @@
+package my.pkg.olegclass;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double distance(Point p){
+        double dx = this.x - p.x;
+        double dy = this.y - p.y;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+}
--- sandbox/src/main/java/my/pkg/olegclass/PointClass.java
+++
@@ -1,7 +0,0 @@
-package my.pkg.olegclass;
-
-public class PointClass {
-    public double x;
-    public double y;
-
-}
