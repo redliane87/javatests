10511f8661998c44cb63114f2079b2b11beae0e4
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-05-20 12:37:57 +0300

    вернул функционал сравнения фактического с ожидаемым результатами

--- sandbox/src/test/java/my/pkg/olegclass/PointTests.java
+++ sandbox/src/test/java/my/pkg/olegclass/PointTests.java
@@ -8,6 +8,6 @@ public class PointTests {
     public void testArea () {
         Point p1 = new Point (1,2);
         Point p2 = new Point (5,5);
-        assert p1.distance(p2) == 5;
+        Assert.assertEquals(p1.distance(p2),5);
     }
 }
