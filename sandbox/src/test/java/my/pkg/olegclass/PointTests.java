03d843967d2b0727d32efca112835c1ef9676f73
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-05-20 12:33:03 +0300

    запуск и успешное прохождение теста с получение фактического и ожидаемого результата

--- sandbox/src/test/java/my/pkg/olegclass/PointTests.java
+++ sandbox/src/test/java/my/pkg/olegclass/PointTests.java
@@ -1,12 +1,13 @@
 package my.pkg.olegclass;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PointTests {
     @Test
     public void testArea () {
-        Point p1 = new Point (1,5);
-        Point p2 = new Point (2,8);
-                assert p1.distance(p2) == 34;
+        Point p1 = new Point (1,2);
+        Point p2 = new Point (5,5);
+        Assert.assertEquals(p1.distance(p2),5);
     }
 }
