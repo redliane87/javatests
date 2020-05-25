5c4c5a36fc6b7f141adf505a9ed1ae37e89894b1
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-05-25 10:33:22 +0300

    Параметизирован метод заполнения формы, создан вспоогательный класс GroupData

--- addressbook-web-tests/src/test/java/my/pkg/addressbook/NewAddressBookCreationTests.java
+++ addressbook-web-tests/src/test/java/my/pkg/addressbook/NewAddressBookCreationTests.java
@@ -14,55 +14,75 @@ public class NewAddressBookCreationTests {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.id("LoginForm")).submit();
   }
 
   @Test
   public void testNewAddressBookCreation() throws Exception {
-    wd.findElement(By.linkText("add new")).click();
+    initAdressCreation("add new");
+    fillAddressForm(new GroupData("Test", "Oleg", "Test", "test11", "Олеко Дундича улица, 7", "redliane87@mail.ru", "redliane87@mail.ru", "redliane87@mail.ru", "15", "October", "1987"));
+  }
+
+  private void fillAddressForm(GroupData groupData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Test");
+    wd.findElement(By.name("firstname")).sendKeys(groupData.getFirstname());
     wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("Oleg");
+    wd.findElement(By.name("middlename")).sendKeys(groupData.getMiddlename());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Test");
+    wd.findElement(By.name("lastname")).sendKeys(groupData.getLastname());
     wd.findElement(By.name("nickname")).click();
     wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("test11");
+    wd.findElement(By.name("nickname")).sendKeys(groupData.getNickname());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("Олеко Дундича улица, 7");
+    wd.findElement(By.name("address")).sendKeys(groupData.getAddress());
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("redliane87@mail.ru");
+    wd.findElement(By.name("email")).sendKeys(groupData.getEmail());
     wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys("redliane87@mail.ru");
+    wd.findElement(By.name("email2")).sendKeys(groupData.getEmail2());
     wd.findElement(By.name("email3")).clear();
-    wd.findElement(By.name("email3")).sendKeys("redliane87@mail.ru");
+    wd.findElement(By.name("email3")).sendKeys(groupData.getEmail3());
     wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText("15");
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(groupData.getBday());
     wd.findElement(By.name("bday")).click();
     wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("October");
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(groupData.getBmonth());
     wd.findElement(By.name("bmonth")).click();
     wd.findElement(By.name("byear")).click();
     wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys("1987");
+    wd.findElement(By.name("byear")).sendKeys(groupData.getByear());
     wd.findElement(By.xpath("//input[21]")).click();
+  }
+
+  private void initAdressCreation(String s) {
+    wd.findElement(By.linkText(s)).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
-    wd.findElement(By.linkText("home")).click();
-    wd.findElement(By.linkText("Logout")).click();
+    gotoHomePage("home");
+    logout("Logout");
     wd.quit();
 
+  }
+
+  private void logout(String logout) {
+    wd.findElement(By.linkText(logout)).click();
+  }
+
+  private void gotoHomePage(String home) {
+    wd.findElement(By.linkText(home)).click();
   }
 
   private boolean isElementPresent(By by) {
