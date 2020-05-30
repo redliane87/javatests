9e6ebd86a15397879686009cf5eb66e90b15d085
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-05-30 11:12:28 +0300

    Создан расчет квадратного уравнения + Тест ( EqationTests) для проверки правильности расчета

---
+++ sandbox/src/main/java/my/pkg/olegclass/Eqation.java
@@ -0,0 +1,33 @@
+package my.pkg.olegclass;
+
+public class Eqation {
+
+    private double a;
+    private double b;
+    private double c;
+
+    private int n;
+
+    public  Eqation(double a, double b, double c){
+
+      this.a = a;
+      this.b = b;
+      this.c = c;
+      double d = b*b - 4*a*c;
+
+      if (d>0) {
+          n = 2;
+      }
+      else {
+          if (d == 0){
+              n=1;
+          }
+          else{
+              n = 0;
+          }
+      }
+    }
+    public int rootNumber (){
+        return n;
+    }
+}
