bb1b494cfe82b0d08c2b749271d333f45c066217
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-06-09 18:19:30 +0300

    изменен метод initModifications для корректной работы модификации контактов (выбора последней строки))

--- addressbook-web-tests/src/test/java/my/pkg/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/my/pkg/addressbook/appmanager/ContactHelper.java
@@ -70,8 +70,9 @@ public class ContactHelper extends HelperBase {
 
     }
 
-    public void initModifications() {
-        click(By.xpath("//img[@alt='Edit']"));
+    public void initModifications(int index) {
+        wd.findElements (By.xpath("//img[@alt='Edit']")).get(index).click();
+
     }
 
     public void submitContactModification() {
