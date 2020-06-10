08a9a7025dd52e69e9981445fdf4d59b858e8b3e
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-06-10 11:06:53 +0300

    в тесте NewContactCreationTest изменено определение максимума со стандартного метода на Лямбда выражение

--- addressbook-web-tests/src/test/java/my/pkg/addressbook/tests/NewContactCreationTest.java
+++ addressbook-web-tests/src/test/java/my/pkg/addressbook/tests/NewContactCreationTest.java
@@ -10,25 +10,20 @@ import java.util.List;
 
 public class NewContactCreationTest extends TestBase {
 
-  @Test
-  public void testNewContactCreation(){
-    List<ContactData> before = app.getContactHelper().getContactList(); // колличество контактов до добавления
-    ContactData contactData = new ContactData("test666", "test2", "test3","123", "test4", "8880978", "redliane@mail.ru", "test001");
-    app.getContactHelper().createContact(contactData, true);
-    app.getNavigationHelper().gotoHomePage();
-    List<ContactData> after = app.getContactHelper().getContactList(); // Колличество контактов после добавления
-    Assert.assertEquals (after.size(), before.size() + 1); // Проверка на колличество контактов до и после добавления
+    @Test
+    public void testNewContactCreation() {
+        List<ContactData> before = app.getContactHelper().getContactList(); // колличество контактов до добавления
+        ContactData contactData = new ContactData("test668", "test4", "test3", "123", "test4", "8880978", "redliane@mail.ru", "test001");
+        app.getContactHelper().createContact(contactData, true);
+        app.getNavigationHelper().gotoHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList(); // Колличество контактов после добавления
+        Assert.assertEquals(after.size(), before.size() + 1); // Проверка на колличество контактов до и после добавления
 
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
+
+        contactData.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        before.add(contactData);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
-    contactData.setId(max);
-    before.add(contactData);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
-  }
 
 
 }
