6ffe5bf1923d1a99c0cb9ca19fc7fcf7ae7704b7
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-06-10 11:03:00 +0300

    в тесте NewGroupCreationTest изменено определение максимума со стандартного метода на Лямбда выражение

--- addressbook-web-tests/src/test/java/my/pkg/addressbook/tests/NewGroupCreationTest.java
+++ addressbook-web-tests/src/test/java/my/pkg/addressbook/tests/NewGroupCreationTest.java
@@ -4,6 +4,7 @@ import my.pkg.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,19 +15,13 @@ public class NewGroupCreationTest extends TestBase {
 
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList(); // колличество групп до добавления. Список
-        GroupData group = new GroupData("test001", null, null);
+        GroupData group = new GroupData("test003", null, null);
         app.getGroupHelper().creatGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList(); // Колличество групп после добавления. Список
         Assert.assertEquals(after.size(), before.size() + 1); // Проверка на колличество групп до и после добавления
 
 
-        int max = 0;
-        for (GroupData q : after) {
-            if (q.getId() > max) {
-                max = q.getId();
-            }
-        }
-        group.setId(max);
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
