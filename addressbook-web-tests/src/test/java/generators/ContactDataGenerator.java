af46ccc1b316777e343a1a2f88edac2909e32370
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-06-22 12:57:59 +0300

    Подключена библиотека Jcommander

--- addressbook-web-tests/src/test/java/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import my.pkg.addressbook.model.ContactData;
 
 import java.io.File;
@@ -10,14 +13,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
+    @Parameter(names = "-c", description = "Group count")
+    public int count;
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
 
     public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
-
-        List<ContactData> contacts = generateContacts(count);
-        save(contacts, file);
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+        generator.run();
     }
+
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts, new File (file));
+    }
+
 
     private static void save(List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
