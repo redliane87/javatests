dfe820035d81333ed17a975e99b7bef24cdd0dba
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-06-22 14:30:56 +0300

    Реализована параметризация тестовых данных для создания Групп и Контактов

--- addressbook-web-tests/src/test/java/my/pkg/addressbook/tests/NewContactCreationTest.java
+++ addressbook-web-tests/src/test/java/my/pkg/addressbook/tests/NewContactCreationTest.java
@@ -8,6 +8,9 @@ import org.testng.annotations.*;
 
 import java.io.File;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,16 +18,22 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class NewContactCreationTest extends TestBase {
 
-    @Test
-    public void testNewContactCreation() {
-        app.goTo().homePage();
+    @DataProvider
+    public Iterator<Object[]> validContacts(){
+        List<Object[]> list = new ArrayList<Object[]>();
         File photo = new File("src/test/resources/mol.jpg");
-        Contacts before = app.contact().all(); // колличество контактов до добавления
-        ContactData contactData
-                = new ContactData().withFName("test668").withLastName("test4").withMidName("test3")
-                .withNickName("123").withMobPhone("8880978").withEmail("redliane@mail.ru").withAddress("testAddress").withGroup("test001").withPhoto(photo);
-        app.contact().create(contactData);
+        list.add(new Object[]{new ContactData().withFName("test668").withLastName("test4").withMidName("test3")
+                .withNickName("123").withMobPhone("8880978").withEmail("redliane@mail.ru").withAddress("testAddress").withGroup("test001").withPhoto(photo)});
+        list.add(new Object[]{new ContactData().withFName("test111").withLastName("test111").withMidName("test111")
+                .withNickName("111").withMobPhone("111").withEmail("redliane1@mail.ru").withAddress("testAddress111").withGroup("test001").withPhoto(photo)});
+        return list.iterator();
+    }
 
+    @Test (dataProvider = "validContacts")
+    public void testNewContactCreation(ContactData contactData) {
+        app.goTo().homePage();
+        Contacts before = app.contact().all(); // колличество контактов до добавления
+        app.contact().create(contactData);
         app.goTo().homePage();
         Contacts after = app.contact().all(); // Колличество контактов после добавления
         assertThat(app.contact().getContactCount(), equalTo(before.size() + 1)); // Проверка на колличество контактов до и после добавления
--- addressbook-web-tests/src/test/java/my/pkg/addressbook/tests/NewGroupCreationTest.java
+++ addressbook-web-tests/src/test/java/my/pkg/addressbook/tests/NewGroupCreationTest.java
@@ -5,21 +5,30 @@ import my.pkg.addressbook.model.Groups;
 import org.testng.annotations.*;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class NewGroupCreationTest extends TestBase {
 
-    @Test
-    public void testNewGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("test1'").withHeader("header 1").withFooter("footer 1")});
+        list.add(new Object[] {new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
+        return list.iterator();
+    }
 
+    @Test (dataProvider = "validGroups")
+    public void testNewGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all(); // колличество групп до добавления. Список
-        GroupData group = new GroupData().withName("test003");
         app.group().create(group);
         Groups after = app.group().all(); // Колличество групп после добавления. Список
         assertThat(app.group().getGroupCount(), equalTo(before.size() + 1)); // Проверка на колличество групп до и после добавления
-
 
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
