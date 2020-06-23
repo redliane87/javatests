f7df75eb682d4e6e0c227f3aa4e68a9608f3f6a3
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-06-23 16:48:37 +0300

    Авто закрытие файлов с помощью try

--- addressbook-web-tests/src/test/java/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/generators/ContactDataGenerator.java
@@ -53,29 +53,31 @@ public class ContactDataGenerator {
 
     private static void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
-        Writer writer = new FileWriter(file);
-        for (ContactData contactData : contacts) {
-            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contactData.getfName(), contactData.getLastName(), contactData.getMidName(), contactData.getNickName(),
-                    contactData.getMobPhone(), contactData.getHomePhone(), contactData.getWorkPhone(), contactData.getEmail(), contactData.getEmail2(), contactData.getEmail3(), contactData.getAddress(), contactData.getGroup()));
+        try (Writer writer = new FileWriter(file)) {
+            for (ContactData contactData : contacts) {
+                writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contactData.getfName(), contactData.getLastName(), contactData.getMidName(), contactData.getNickName(),
+                        contactData.getMobPhone(), contactData.getHomePhone(), contactData.getWorkPhone(), contactData.getEmail(), contactData.getEmail2(), contactData.getEmail3(), contactData.getAddress(), contactData.getGroup()));
+            }
         }
-        writer.close();
     }
 
     private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
         XStream xStream = new XStream();
         xStream.processAnnotations(ContactData.class);
         String xml = xStream.toXML(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
+
     }
 
     private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
         String json = gson.toJson(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(json);
-        writer.close();
+        try (Writer writer = new FileWriter(file)) {
+            writer.write(json);
+        }
+
     }
 
     private static List<ContactData> generateContacts(int count) {
--- addressbook-web-tests/src/test/java/my/pkg/addressbook/tests/NewContactCreationTest.java
+++ addressbook-web-tests/src/test/java/my/pkg/addressbook/tests/NewContactCreationTest.java
@@ -29,31 +29,34 @@ public class NewContactCreationTest extends TestBase {
     @DataProvider
     public Iterator<Object[]> validContactsXml() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        File photo = new File("src/test/resources/mol.jpg");
-        BufferedReader reader = new BufferedReader(new FileReader(new File ("src/test/resources/contacts.xml")));
-        String xml = "";
-        String line = reader.readLine();
-        while (line != null){
-            xml += line;
-            line = reader.readLine();
+       // File photo = new File("src/test/resources/mol.jpg");
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File ("src/test/resources/contacts.xml")))){
+            String xml = "";
+            String line = reader.readLine();
+            while (line != null){
+                xml += line;
+                line = reader.readLine();
+            }
+            XStream xStream = new XStream();
+            xStream.processAnnotations(ContactData.class);
+            List<ContactData> contacts = (List<ContactData>) xStream.fromXML(xml);
+            return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
         }
-        XStream xStream = new XStream();
-        xStream.processAnnotations(ContactData.class);
-        List<ContactData> contacts = (List<ContactData>) xStream.fromXML(xml);
-        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
+
     @DataProvider
     public Iterator<Object[]> validContactsJson() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File ("src/test/resources/contacts.json")));
-        String json = "";
-        String line = reader.readLine();
-        while (line != null){
-            json += line;
-            line = reader.readLine();
-        }
-        Gson gson = new Gson();
-        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType()); // List<ContactData>.class
-        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+       try (BufferedReader reader = new BufferedReader(new FileReader(new File ("src/test/resources/contacts.json")))){
+           String json = "";
+           String line = reader.readLine();
+           while (line != null){
+               json += line;
+               line = reader.readLine();
+           }
+           Gson gson = new Gson();
+           List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType()); // List<ContactData>.class
+           return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+       }
     }
 
     @Test (dataProvider = "validContactsJson")
