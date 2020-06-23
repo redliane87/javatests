9b156bd6381957c7f1e003bc302cd62bf320515a
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-06-23 09:01:42 +0300

    Реализовано создание файла в формате xml и чтенение файла в тесте по созданию Групп

--- addressbook-web-tests/src/test/java/generators/GroupDataGenerators.java
+++ addressbook-web-tests/src/test/java/generators/GroupDataGenerators.java
@@ -3,6 +3,7 @@ package generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import my.pkg.addressbook.model.GroupData;
 
 import java.io.File;
@@ -17,6 +18,8 @@ public class GroupDataGenerators {
     public int count;
     @Parameter (names = "-f", description = "Target file")
     public String file;
+    @Parameter (names = "-d", description = "Data format")
+    public String format;
 
     public static void main(String[] args) throws IOException {
         GroupDataGenerators generator = new GroupDataGenerators();
@@ -32,10 +35,26 @@ public class GroupDataGenerators {
 
     private void run() throws IOException {
         List<GroupData> groups = generateGroups(count);
-        save(groups, new File(file));
+        if (format.equals("csv")){
+            saveAsCsv(groups, new File(file));
+        } else if (format.equals("xml")){
+            saveAsXml(groups, new File(file));
+        } else {
+            System.out.println("Unrecognized format " + format);
+        }
+
     }
 
-    private void save(List<GroupData> groups, File file) throws IOException {
+    private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+        XStream xStream = new XStream();
+        xStream.processAnnotations(GroupData.class);
+        String xml = xStream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private void saveAsCsv(List<GroupData> groups, File file) throws IOException {
         System.out.println(new File(".").getAbsolutePath());
         Writer writer = new FileWriter(file);
         for (GroupData group : groups) {
@@ -43,6 +62,7 @@ public class GroupDataGenerators {
         }
         writer.close();
     }
+    
 
     private List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
