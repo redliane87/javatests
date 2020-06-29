f1759be894703b5f61ba4720bc310cc89b7dd8ac
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-06-29 13:24:04 +0300

    Реализовано получение данных по контактам с помощью OPM Hibernate (с доп., условиями отбора данных) допушил

--- addressbook-web-tests/src/test/java/my/pkg/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/my/pkg/addressbook/model/ContactData.java
@@ -3,39 +3,69 @@ package my.pkg.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
 import java.io.File;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
     @XStreamOmitField
+    @Id
+    @Column(name = "id")
     private int id = Integer.MAX_VALUE;
     @Expose
+    @Column(name = "firstname")
     private String fName;
     @Expose
+    @Column(name = "middlename")
     private String midName;
     @Expose
+    @Column(name = "lastname")
     private String lastName;
     @Expose
+    @Column(name = "nickname")
     private String nickName;
     @Expose
+    @Column(name = "address")
+    @Type(type = "text")
     private String address;
     @Expose
+    @Column(name = "mobile")
+    @Type(type = "text")
     private String mobPhone;
     @Expose
+    @Column(name = "home")
+    @Type(type = "text")
     private String homePhone;
     @Expose
+    @Column(name = "work")
+    @Type(type = "text")
     private String workPhone;
+    @Transient
     private String allphones;
     @Expose
+    @Column(name = "email")
+    @Type(type = "text")
     private String email;
     @Expose
+    @Column(name = "email2")
+    @Type(type = "text")
     private String email2;
     @Expose
+    @Column(name = "email3")
+    @Type(type = "text")
     private String email3;
+    @Transient
     private String allemails;
     @Expose
+    @Transient
     private String group;
-    private File photo;
+    @Column(name = "photo")
+    @Type(type = "text")
+    private String photo;
 
 
     public int getId() {
@@ -118,12 +148,12 @@ public class ContactData {
         return this;
     }
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 
     public File getPhoto() {
-        return photo;
+        return new File (photo);
     }
 
     public String getfName() {
