08e9fc30479b614942700f5a8de060bc0f143d88
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-06-29 16:49:58 +0300

    добавлена проверка на наличие файла

--- addressbook-web-tests/src/test/java/my/pkg/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/my/pkg/addressbook/model/ContactData.java
@@ -153,7 +153,12 @@ public class ContactData {
     }
 
     public File getPhoto() {
-        return new File (photo);
+        if (photo != null) {
+            return new File(photo);
+        } else {
+            return null;
+        }
+
     }
 
     public String getfName() {
