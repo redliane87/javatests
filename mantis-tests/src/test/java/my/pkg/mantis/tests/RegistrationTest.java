aa4ddc3ea824ba72f507f88d6e84d139a6123457
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-07-07 14:55:05 +0300

    8.7 добавлен тест на регистрацию с использованием внешнего почтового сервера

--- mantis-tests/src/test/java/my/pkg/mantis/tests/RegistrationTest.java
+++ mantis-tests/src/test/java/my/pkg/mantis/tests/RegistrationTest.java
@@ -7,25 +7,28 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 
+import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
 
 import static org.testng.Assert.assertTrue;
 
 public class RegistrationTest extends TestBase {
-    @BeforeMethod
+    //@BeforeMethod
     public void startMailServer() {
         app.mail().start();
     }
 
     @Test
-    public void testRegistration() throws IOException {
+    public void testRegistration() throws IOException, MessagingException {
         long now = System.currentTimeMillis();
         String user = String.format("user%s", now);
         String password = "password";
-        String email = String.format("user%s@localhost.localdomein", now);
+        String email = String.format("user%s@localhost", now);
+        app.james().createUser(user, password);
         app.registration().start(user, email);
-        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+       // List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        List<MailMessage> mailMessages = app.james().waitForMail(user,password,  600000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         app.registration().finish(confirmationLink, password);
         assertTrue(app.newSession().login(user, password));
@@ -38,7 +41,7 @@ public class RegistrationTest extends TestBase {
     }
 
 
-    @AfterMethod(alwaysRun = true)
+    //@AfterMethod(alwaysRun = true)
     public void stopMailServer() {
         app.mail().stop();
     }
