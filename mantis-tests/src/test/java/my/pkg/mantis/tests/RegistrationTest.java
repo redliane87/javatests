6d3d9e7ba376ab8a816d8901aa8e963f75ee3a05
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-07-07 08:40:36 +0300

    Лекция 8.4. Ленивая инициализация помощников

---
+++ mantis-tests/src/test/java/my/pkg/mantis/tests/RegistrationTest.java
@@ -0,0 +1,11 @@
+package my.pkg.mantis.tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTest extends TestBase {
+
+    @Test
+    public void testRegistration(){
+        app.registration().start("user1", "user1@localhost.localdomein");
+    }
+}
