2517fba41d99194c59ebaaedcb70d3820c52c557
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-07-27 16:54:05 +0300

    Реадизована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/my/pkg/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/my/pkg/addressbook/appmanager/ApplicationManager.java
@@ -1,16 +1,20 @@
 package my.pkg.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -36,6 +40,7 @@ public class ApplicationManager {
 
         dbHelper = new DbHelper();
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+       if("".equals(properties.getProperty("selenium.server"))){
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)){
@@ -43,6 +48,11 @@ public class ApplicationManager {
         } else if (browser.equals(BrowserType.IE)){
             wd = new InternetExplorerDriver();
         }
+       } else {
+           DesiredCapabilities capabilities = new DesiredCapabilities();
+           capabilities.setBrowserName(browser);
+           wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+       }
        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
        wd.get(properties.getProperty("web.baseUrl"));
        groupHelper = new GroupHelper(wd);
