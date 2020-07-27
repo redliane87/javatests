4294646ad464e79b01a557d41c56271a3cec9f2f
redliane87 <65483653+redliane87@users.noreply.github.com>
2020-07-27 12:15:25 +0300

    9.5 add rest-assured test

---
+++ rest-sample/src/test/java/my/pkg/rest/tests/RestAssuredTests.java
@@ -0,0 +1,49 @@
+package my.pkg.rest.tests;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+public class RestAssuredTests {
+
+    @BeforeClass
+    public void init() {
+        RestAssured.authentication = RestAssured.basic("288f44776e7bec4bf44fdfeb1e646490", "");
+    }
+
+    @Test
+    public void testCreateIssue() throws IOException {
+        Set<Issue> oldIssues = getIssues();
+        Issue newIssue = new Issue().withSubject("test nik").withDescription("test nik description");
+        int issueId = createIssue(newIssue);
+        Set<Issue> newIssues = getIssues();
+        oldIssues.add(newIssue.withId(issueId));
+        assertEquals(newIssues, oldIssues);
+    }
+
+    private Set<Issue> getIssues() {
+        String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json?page=100&limit=2000").asString();
+        JsonElement parsed = JsonParser.parseString(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {
+        }.getType());
+    }
+
+    private int createIssue(Issue newIssue) {
+        String json = RestAssured.given().
+                param("subject", newIssue.getSubject()).
+                param("description", newIssue.getDescription()).
+                post("https://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = JsonParser.parseString(json);
+        return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+}
